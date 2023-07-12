66273d7ab56b3c7ed7aa199798b0267aad93d044
MehrdadMoradi001 <moradi.mehrdad001@gmail.com>
2023-07-12 21:49:43 +0330

    1- Added flutter_generate 2- Added Icons, images, fonts 3- Added localizations

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      localizationsDelegates: [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: [
+        Locale('fa'), // farsi
+      ],
+      home: Scaffold(
+        body: Text('dsdds'),
+      ),
+    );
+  }
+}
