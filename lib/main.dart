8b23a7184076ced4079fdf7549ce4fd731365738
MehrdadMoradi001 <moradi.mehrdad001@gmail.com>
2023-07-13 04:56:28 +0330

    1- SystemChrome was set for statusBar and NavigationBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:tech_media/component/my_colors.dart';
 import 'package:tech_media/view/splash_screen.dart';
 
 void main() {
+  //SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light);
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+    statusBarColor: SolidColors.statusBarColor,
+    statusBarIconBrightness: Brightness.dark,
+    systemNavigationBarColor: SolidColors.systemNavigationBarColor,
+    systemNavigationBarIconBrightness: Brightness.dark,
+  ));
+
   runApp(const MyApp());
 }
 
