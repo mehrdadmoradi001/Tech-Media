be145859bd4acdd9d04edc8104b987d919652f04
MehrdadMoradi001 <moradi.mehrdad001@gmail.com>
2023-07-19 23:46:38 +0330

    1- created head profile

--- lib/view/profile_screen.dart
+++ lib/view/profile_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:tech_media/component/my_colors.dart';
+import 'package:tech_media/component/my_strings.dart';
+import 'package:tech_media/gen/assets.gen.dart';
 
 import '../gen/assets.gen.dart';
 
@@ -18,9 +20,21 @@ class ProfileScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const SingleChildScrollView(
-      physics:  BouncingScrollPhysics(),
-      child: Text('Profile Screen'),
+    return  SingleChildScrollView(
+      physics:  const BouncingScrollPhysics(),
+      child: Column(
+        children: [
+
+          const SizedBox(height: 12),
+          Row(
+            children: [
+              ImageIcon(Assets.icons.bluePen.provider(),color: SolidColors.seeMore,),
+              const SizedBox(width: 8),
+              Text(MyStrings.imageProfileEdit,style: textTheme.headlineMedium,),
+            ],
+          )
+        ],
+      ),
     );
   }
 }
