1704efb090f5b8358ba7f5fa6804007827c3d566
MehrdadMoradi001 <moradi.mehrdad001@gmail.com>
2023-08-06 12:51:23 +0330

    refactor: removing const

--- lib/main.dart
+++ lib/main.dart
@@ -105,7 +105,7 @@ class MyApp extends StatelessWidget {
               color: SolidColors.blackColor),
         ),
       ),
-      home: const MainScreen(),
+      home:  MainScreen(),
     );
   }
 }
