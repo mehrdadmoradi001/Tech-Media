c74041a330383d01200637f2542f5ebde02a52d3
MehrdadMoradi001 <moradi.mehrdad001@gmail.com>
2023-08-06 12:51:11 +0330

    refactor: removing const

--- lib/view/splash_screen.dart
+++ lib/view/splash_screen.dart
@@ -19,7 +19,7 @@ class _SplashScreenState extends State<SplashScreen> {
     Future.delayed(const Duration(seconds: 3)).then((value) {
       Navigator.of(context).pushReplacement(
         MaterialPageRoute(
-          builder: (context) => const MainScreen(),
+          builder: (context) =>  MainScreen(),
         ),
       );
     });
